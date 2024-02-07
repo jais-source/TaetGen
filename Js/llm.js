34a177ade722c2202c5100e08d353057f76a64e2
jais-source <159273210+jais-source@users.noreply.github.com>
2024-02-07 14:29:51 -0400

    Update llm.js

--- Js/llm.js
+++ Js/llm.js
@@ -15,7 +15,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
         resultadoConsulta.innerHTML = text
         sessionStorage.setItem('respuesta', text);
 
-        window.location.href = "../Html/Generado.html";
+        window.location.href = "../index.html";
    
       } catch (error) {
         resultadoConsulta.innerHTML = 'Problemas en la consulta'
